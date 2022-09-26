67211ed75537c8de84ce1f2f9b8a6401ce38dbb5
Георгий Александрович Мичкасов <michkasov.ga1668@oksei.onmicrosoft.com>
2022-09-26 22:13:14 +0500

    test edit

---
+++ PZ_6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PZ_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вариант 18");
+            double x, y, h;
+            x = 1;
+            h = 0.2;
+            while (1 <= x && x <= 4)
+            {
+                y = x + 3 * Math.Pow(2, x) - 2.5 * Math.Pow(3, x) + 0.75 * Math.Pow(3, x);
+                Console.WriteLine($"y = {y}");
+                x += h;
+            }
+        }
+    }
+}
