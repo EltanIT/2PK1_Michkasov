d81ca493604c932e06f5488c56aeef0db82a2f57
Георгий Александрович Мичкасов <michkasov.ga1668@oksei.onmicrosoft.com>
2022-10-26 18:20:32 +0500

    test edit

---
+++ PZ_10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PZ_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = Console.ReadLine();
+            var result1 = new Regex("[0-9]+").Matches(str);
+            foreach (var match in result1) 
+                Console.WriteLine(match);
+        }
+    }
+}
