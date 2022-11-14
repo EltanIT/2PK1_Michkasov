bd089d7bc86f9b26bc95bf917f4eb0ccae2e84f9
Георгий Александрович Мичкасов <michkasov.ga1668@oksei.onmicrosoft.com>
2022-11-14 18:03:47 +0500

    test edit

---
+++ PZ_12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PZ_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите колличество раз выполнения программы(3)");
+            int a = int.Parse(Console.ReadLine());
+            int bol = 1;
+        restart:
+            Console.WriteLine("Введите значения координат для определения прямоугольника: x1,y1,x2,y2");
+            double x1 = int.Parse(Console.ReadLine());
+            double y1 = int.Parse(Console.ReadLine());
+            double x2 = int.Parse(Console.ReadLine());
+            double y2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"x1={x1},  y1={y1},  x2={x2},  y2={y2}");
+
+            RectPS(x1, y1, x2, y2);
+
+            if ((bol++) >= a) Console.WriteLine("На этом все");
+            else goto restart;
+
+        }
+        static void RectPS(double x1, double y1, double x2, double y2)
+        {
+            double P = (Math.Abs((x1 - x2)) * 2) + (Math.Abs(y1 - y2) * 2);
+            double S = (Math.Abs(x1 - x2)) + (Math.Abs(y1 - y2));
+            Console.WriteLine($"P = {P}\t S = {S}");
+        }
+    }
+}
---
+++ PZ_13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PZ_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение n = ");
+            int n = int.Parse(Console.ReadLine());
+            int[] result = TransArr(n);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static int[] TransArr(int n)
+        {
+            int[] arr = new int[n];
+
+            int g = n;
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = 10 * (int)Math.Pow(2, g - 1);
+                g++;
+            }
+            return arr;
+        }
+    }
+}
