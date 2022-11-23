82bf051011de9a54cbfc536d49ac65b3c636ac70
Георгий Александрович Мичкасов <michkasov.ga1668@oksei.onmicrosoft.com>
2022-11-23 13:29:32 +0500

    text edit

---
+++ PZ_23.11.22/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace PZ_23._11._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fileStream = new FileStream(@"D:\text.txt",FileMode.Append);
+
+            Console.WriteLine("Введите произвольную строку в ваш файл:");
+            string str = Console.ReadLine();
+            StreamWriter streamwriter = new StreamWriter(fileStream);
+            streamwriter.Write(str);
+            streamwriter.Close(); 
+
+            FileStream fileStream2 = new FileStream(@"D:\text.txt", FileMode.OpenOrCreate);
+            StreamReader streamReader = new StreamReader(fileStream2);
+            string str2 = streamReader.ReadToEnd();
+            streamReader.Close();
+            Console.WriteLine($"Содержимое вашего файла:{str2}");
+        }
+    }
+}
