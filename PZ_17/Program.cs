e25bc27a956026a899952e3e154d244d85b70044
Георгий Александрович Мичкасов <michkasov.ga1668@oksei.onmicrosoft.com>
2022-12-15 00:22:34 +0500

    test edit

---
+++ PZ_17/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PZ_17
+{
+    class Program
+    {
+       unsafe static void Main(string[] args)
+        {
+            double Double = 10;
+            double* p = &Double;
+            int DoubleAdress = (int)p;
+
+            string result = DoubleAdress.ToString();
+            char[] resChar = result.ToCharArray();
+
+            Console.WriteLine(DoubleAdress);
+
+            byte[] Byte = new byte[result.Length];
+
+
+            for (int i = 0; i < Byte.Length; i++)
+            {
+                fixed(byte* tt = Byte)
+                {
+                    tt[i] = (byte)resChar[i];
+                }
+            }
+            Console.WriteLine((byte)Byte[0]); 
+
+        }
+    }
+}
