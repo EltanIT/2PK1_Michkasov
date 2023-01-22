9fdac25f48140d0fe3aae467045740455ffa1a91
Георгий Александрович Мичкасов <michkasov.ga1668@oksei.onmicrosoft.com>
2023-01-22 22:33:54 +0500

    ау

---
+++ PZ_20/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PZ_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string text = Console.ReadLine();
+            StringRead(text);
+        }
+
+        static void StringRead(string text)
+        {
+            string result = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsUpper(text[i]))
+                    result += text[i].ToString().ToLower();
+                else
+                    result += text[i].ToString().ToUpper();
+                
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
